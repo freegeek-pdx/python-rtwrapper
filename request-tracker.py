207de328eced6e77a37ce797f8144429039d5de3
paulm <paulm@freegeek.org>
2013-03-29 11:10:14 -0700

    bugfixed

--- request-tracker.py
+++ request-tracker.py
@@ -2,8 +2,8 @@
 import unittest
 #from  rt import RT as RequestTracker
 import rt
-import timedate
-from timdate import timedelta
+import datetime
+from datetime import timedelta
 
 ########################################
 #           Configuration              #
@@ -22,32 +22,29 @@ rt_url='http://' + rt_host + '/REST/1.0/'
 #        pass
 
 class MyTests(unittest.TestCase):
-    def __init__(rqt):
-        self.rqt = rqt
+    def setUp(self):
+        self.rqt = RT(rt_url, rt_user, rt_password)
+        self.rqt.login()
+
     def test_is_valid_ticket(self):
-        result = rqt.is_valid_ticket('34716')
+        result = self.rqt.is_valid_ticket('34716')
         self.assertTrue(result)
 
     def test_is_active_ticket(self):
-        result = rqt.is_active_ticket('34716')
+        result = self.rqt.is_active_ticket('34716')
         self.assertTrue(result)
     
     def test_is_active_ticket_false(self):
-        result = rqt.is_active_ticket('21950')
+        result = self.rqt.is_active_ticket('21950')
         self.assertFalse(result)
 
-### FUNCTIONS ###
-
-#def login():
-    # return success/fail
-
-class RT(RequestTracker):
-
+class RT(rt.Rt):
+    '''Extends rt. Provides additional functions''' 
     def is_valid_ticket(self, ticket):
         ''' Returns true if ticket number supplied exists in the 
         tech support queue'''
         try:
-            search_results = rqt.search('TechSupport',id=ticket)
+            search_results = self.search('TechSupport',id=ticket)
             if len(search_results) > 0:
                 return True
             else:
@@ -62,11 +59,11 @@ class RT(RequestTracker):
         # so this is a double test, checking if resolved
         # if not check it is in tech support
         try:
-            search_results = rqt.search('TechSupport',status='resolved', id=ticket)
+            search_results = self.search('TechSupport',status='resolved', id=ticket)
             if len(search_results) > 0:
                 is_not_resolved = False
             else:
-                search_results = rqt.search('TechSupport', id=ticket)
+                search_results = self.search('TechSupport', id=ticket)
                 if len(search_results) > 0:
                     is_not_resolved = True
             return is_not_resolved
@@ -79,11 +76,9 @@ class RT(RequestTracker):
         today = datetime.datetime.today()
         timedelta = datetime.timedelta(days)
         cutoff = today - timedelta
-        search_results = rqt.search('TechSupport', status=statustype, LastUpdated<cutoff)
+        #search_results = self.search('TechSupport', status=statustype, LastUpdated<cutoff)
         # return list of ids
 
 if __name__ == "__main__":
-    tracker = RT(rt_url, rt_user, rt_password)
-    tracker.login()
-    unittest.main(tracker)
+    unittest.main()
 

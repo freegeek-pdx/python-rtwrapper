3b2f3fd7bcef5261b4e59f876347bfaea80c8fe7
paulm <paulm@freegeek.org>
2013-03-28 20:01:12 -0700

    made more OO

--- request-tracker.py
+++ request-tracker.py
@@ -2,6 +2,9 @@
 import unittest
 #from  rt import RT as RequestTracker
 import rt
+import timedate
+from timdate import timedelta
+
 ########################################
 #           Configuration              #
 ########################################
@@ -19,60 +22,68 @@ rt_url='http://' + rt_host + '/REST/1.0/'
 #        pass
 
 class MyTests(unittest.TestCase):
+    def __init__(rqt):
+        self.rqt = rqt
     def test_is_valid_ticket(self):
-        result = is_valid_ticket('34716')
+        result = rqt.is_valid_ticket('34716')
         self.assertTrue(result)
 
     def test_is_active_ticket(self):
-        result = is_active_ticket('34716')
+        result = rqt.is_active_ticket('34716')
         self.assertTrue(result)
     
-    def test_is_active_ticketi_false(self):
-        result = is_active_ticket('21950')
+    def test_is_active_ticket_false(self):
+        result = rqt.is_active_ticket('21950')
         self.assertFalse(result)
 
 ### FUNCTIONS ###
 
-def is_valid_ticket(ticket):
-    ''' Returns true if ticket number supplied exists in the 
-    tech support queue'''
-    rqt = rt.Rt(rt_url, rt_user, rt_password)
-    rqt.login()
-    try:
-        search_results = rqt.search('TechSupport',id=ticket)
-        if len(search_results) > 0:
-            return True
-        else:
-            return False
-    except:
-            return False
+#def login():
+    # return success/fail
 
-def is_active_ticket(ticket):
-    '''Returns true if ticket number supplied exists in the 
-    tech support queue and is not resolved'''
-    rqt = rt.Rt(rt_url, rt_user, rt_password)
-    rqt.login()
-    # the rt module doesn't work with e.g. Status!='resolved'
-    # so this is a double test, checking if resolved
-    # if not check it is in tech support
-    try:
-        search_results = rqt.search('TechSupport',status='resolved', id=ticket)
-        if len(search_results) > 0:
-            is_not_resolved = False
-        else:
-            search_results = rqt.search('TechSupport', id=ticket)
+class RT(RequestTracker):
+
+    def is_valid_ticket(self, ticket):
+        ''' Returns true if ticket number supplied exists in the 
+        tech support queue'''
+        try:
+            search_results = rqt.search('TechSupport',id=ticket)
             if len(search_results) > 0:
-                is_not_resolved = True
-        return is_not_resolved
-    except:
-            return False
+                return True
+            else:
+                return False
+        except:
+                return False
+
+    def is_active_ticket(self, ticket):
+        '''Returns true if ticket number supplied exists in the 
+        tech support queue and is not resolved'''
+        # the rt module doesn't work with e.g. Status!='resolved'
+        # so this is a double test, checking if resolved
+        # if not check it is in tech support
+        try:
+            search_results = rqt.search('TechSupport',status='resolved', id=ticket)
+            if len(search_results) > 0:
+                is_not_resolved = False
+            else:
+                search_results = rqt.search('TechSupport', id=ticket)
+                if len(search_results) > 0:
+                    is_not_resolved = True
+            return is_not_resolved
+        except:
+                return False
 
 
-def is_older_than(status, days):
-    # Status = 'contact' AND LastUpdated < '2013-03-27' AND Queue = 'TechSupport'
-    rqt = rt.Rt(rt_url, rt_user, rt_password)
-    rqt.login()
+    def is_older_than(self, statustype, days):
+        # Status = 'contact' AND LastUpdated < '2013-03-27' AND Queue = 'TechSupport'
+        today = datetime.datetime.today()
+        timedelta = datetime.timedelta(days)
+        cutoff = today - timedelta
+        search_results = rqt.search('TechSupport', status=statustype, LastUpdated<cutoff)
+        # return list of ids
 
 if __name__ == "__main__":
-    unittest.main()
+    tracker = RT(rt_url, rt_user, rt_password)
+    tracker.login()
+    unittest.main(tracker)
 

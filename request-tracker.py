adbf5e196d3522847e8461ef5f3416bd1e91bdec
paulm <paulm@freegeek.org>
2013-04-20 13:14:28 -0700

    fixed asearch for spaces in search

--- request-tracker.py
+++ request-tracker.py
@@ -78,8 +78,24 @@ class MyTests(unittest.TestCase):
             has_results = False
         self.assertTrue(has_results)
 
-    def test_is_older(self):
-        result = self.rqt.is_older_than(self.rt_queue, 'pending', 3)
+    def test_last_updated_by_status(self):
+        result = self.rqt.last_updated_by_status(self.rt_queue, 'pending', 3)
+        if len(result)>0:
+            has_results = True
+        else:
+            has_results = False
+        self.assertTrue(has_results)
+
+    def test_last_updated_by_field(self):
+        result = self.rqt.last_updated_by_field(self.rt_queue, 'new', 'Owner', 'nobody', 0)
+        if len(result)>0:
+            has_results = True
+        else:
+            has_results = False
+        self.assertTrue(has_results)
+
+    def test_last_updated_by_field_custom(self):
+        result = self.rqt.last_updated_by_field(self.rt_queue, 'open', 'CF.{Ticket Source}', 'Box Brought In', 0) 
         if len(result)>0:
             has_results = True
         else:
@@ -87,7 +103,7 @@ class MyTests(unittest.TestCase):
         self.assertTrue(has_results)
 
     def test_format_results(self):
-        older = self.rqt.is_older_than(self.rt_queue, 'pending', 3)
+        older = self.rqt.last_updated_by_status(self.rt_queue, 'pending', 3)
         result = format_results(older, 'id', 'Subject')
         if len(result)>0:
             has_results = True
@@ -103,7 +119,7 @@ class MyTests(unittest.TestCase):
     # this passes, commenting out to avoid having to wait for user prompt
     '''
     def test_email_results(self):
-        older = self.rqt.is_older_than(self.rt_queue, 'pending', 3)
+        older = self.rqt.last_updated_by_status(self.rt_queue, 'pending', 3)
         result = format_results(older, 'id', 'Subject')
         #email = raw_input('Enter an email address to send a message to: ')
         email_results(self.mailhost, self.email, self.email, 'Unit Test', 
@@ -156,6 +172,7 @@ class MyTests(unittest.TestCase):
 class RT(rt.Rt):
     # pylint: disable=R0904
     '''Extends rt.Rt Provides additional functions'''
+    
 
     def asearch(self, queue, *args):
         """ Search in queue using arbitary strings so that you can
@@ -178,7 +195,8 @@ class RT(rt.Rt):
         """
         query = 'search/ticket?query=(Queue=\'%s\')' % (queue,)
         for item in args:
-            query += "AND%s" % item
+            item = item.replace(" ","+")
+            query += "+AND+(%s)" % item
         query += "&format=l"
         # Accessing a private method from the parent here
         # but whatever, I'm not going to write my own 
@@ -186,7 +204,6 @@ class RT(rt.Rt):
         # that should go in the parent module.
         msgs = self._Rt__request(query)
         msgs = msgs.split('\n--\n')
-        
         items = []
         try:
             if not hasattr(self, 'requestors_pattern'):
@@ -250,9 +267,9 @@ class RT(rt.Rt):
         except:
             return False
 
-    def is_older_than(self, queue, statustype, days):
+    def last_updated_by_status(self, queue, statustype, days):
         '''Returns a list of tickets (i.e. id, Subject etc)
-        with status [statustype], Last updated  [days] days ago '''
+        with status [statustype], last updated  [days] days ago '''
         today = datetime.date.today()
         tdelta = datetime.timedelta(days)
         cutoff = today - tdelta
@@ -260,6 +277,25 @@ class RT(rt.Rt):
         search_results = self.asearch(queue, search_string) 
         return search_results
 
+    def last_updated_by_field(self, queue, statustype, field, fieldtype, days):
+        '''Returns a list of tickets (i.e. id, Subject etc)
+        by field, last updated  [days] days ago. Status type can be active '''
+        today = datetime.date.today()
+        tdelta = datetime.timedelta(days)
+        cutoff = today - tdelta
+        if statustype == 'active' or statustype == 'Active':
+            status_string = '(Status=\'new\' OR Status=\'open\' OR Status=\'stalled\' OR Status=\'pending\' OR Status=\'contact\')' 
+        else:
+            status_string = 'Status=\'' + statustype + '\''
+        regex = re.compile('CF')
+        if regex.match(field):
+            field_string = '\'' + field + '\'' + '=\'' + fieldtype + '\''
+        else:
+            field_string = '\'' + field + '\''+ '=\'' + fieldtype + '\''
+        updated_string = 'LastUpdated<\'' + str(cutoff) + '\''
+        search_results = self.asearch(queue, status_string, field_string, updated_string) 
+        return search_results
+    
     def get_status(self, ticket_id):
         '''returns status of ticket'''
         ticket = self.get_ticket(ticket_id)
@@ -361,7 +397,13 @@ if __name__ == "__main__":
     email = mail['email']
     mailhost = mail['mail_host']
     rqt = RT(RT_URL, rt_user, rt_password)
-    #print(rqt.login())
+    print(rqt.login())
     #print(rqt.get_status(34716))
     #print(rqt.add_comment(34716,'test'))
+    
+    #print(rqt.last_updated_by_field(rt_queue, 'open', 'CF.{Ticket+Source}', 'Box+Brought+In', 0))
+    #rqt.last_updated_by_field(rt_queue, 'new', 'Owner', 'nobody', 0)
+    #print(rqt.search(Queue='TechSupport', Status='open', CF_='Box Brought In'))
+    
+    #print(rqt.last_updated_by_field(rt_queue, 'new', 'Owner', 'nobody', 0))
     #print(rqt.get_status(34716))
